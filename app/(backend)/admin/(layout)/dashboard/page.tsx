2af0b2dd92b06039783524ad9ca82ff8f2513934
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-11-08 18:20:44 +0600

    authenticated

---
+++ app/(backend)/admin/(layout)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminDashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold">Admin Dashboard</h1>
+    </div>
+  );
+}
